7e79522bc46e9a2d973cc2c36de9855c03363c14
Joshua Marketis <josh@clidus.com>
2014-07-02 19:43:59 +0100

    Rename system and application folders

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ if (defined('ENVIRONMENT'))
  * as this file.
  *
  */
-	$system_path = 'gwl_system';
+	$system_path = 'ignition_system';
 
 /*
  *---------------------------------------------------------------
@@ -72,7 +72,7 @@ if (defined('ENVIRONMENT'))
  * NO TRAILING SLASH!
  *
  */
-	$application_folder = 'gwl_application';
+	$application_folder = 'ignition_application';
 
 /*
  * --------------------------------------------------------------------
