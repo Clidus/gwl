0e04379303ccf40fad38a22a94ce0d90f6db1cc2
Joshua Marketis <josh@clidus.com>
2014-07-06 19:32:33 +0100

    Missed reference to application folder name

--- ignition_application/controllers/pages.php
+++ ignition_application/controllers/pages.php
@@ -5,7 +5,7 @@ class Pages extends CI_Controller {
 	// view static page
 	public function view($page = 'home')
 	{
-		if ( ! file_exists('gwl_application/views/pages/'.$page.'.php'))
+		if ( ! file_exists('ignition_application/views/pages/'.$page.'.php'))
 		{
 			// Whoops, we don't have a page for that!
 			show_404();
