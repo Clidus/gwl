95c170086885c60ca2c91b7874eb7c59990a977e
Joshua Marketis <josh@clidus.com>
2014-07-09 09:49:14 +0100

    Update change log

--- ignition_application/views/pages/changelog.php
+++ ignition_application/views/pages/changelog.php
@@ -6,6 +6,22 @@
 <h1><?php echo $pagetitle ?></h1>
 
 <div>
+	<p><b>Version 0.3.0 - Unreleased</b></p>
+	<ul>
+		<li><strong>Feature:</strong> You can now follow users. <a href="https://github.com/Clidus/gwl/issues/50">#29</a></li>
+		<li><strong>Feature:</strong> You can see what you and your followers have done recently in a new homepage feed. <a href="https://github.com/Clidus/gwl/issues/50">#29</a></li>
+		<li><strong>Feature:</strong> HTTPS is now supported and enforced. <a href="https://github.com/Clidus/gwl/issues/50">#50</a></li>
+		<li><strong>Feature:</strong> Comments on blog posts. <a href="https://github.com/Clidus/gwl/issues/12">#12</a></li>
+		<li><strong>Feature:</strong> Blog post archive.</li>
+		<li><strong>Change:</strong> Making a comment on an event will now bump it to the top of the feed.</li>
+		<li><strong>Change:</strong> New look for events.</li>
+		<li><strong>Change:</strong> New look for the blog.</li>
+		<li><strong>Change:</strong> Uncompletable games are no longer included in the completion percentage. <a href="https://github.com/Clidus/gwl/issues/52">#52</a></li>
+		<li><strong>Change:</strong> New site footer with social links.</li>
+		<li><strong>Change:</strong> Upgraded bootstrap from v3.0 to v3.2.</li>
+		<li><strong>Bug Fix:</strong> There was a missing "and" when an event contained "is playing" and one status. <a href="https://github.com/Clidus/gwl/issues/51">#51</a></li>
+		<li><strong>Bug Fix:</strong> Some errors returned no message.</li>
+	</ul>
 	<p><b>Version 0.2.3 - 2014-06-18</b></p>
 	<ul>
 		<li><strong>Change:</strong> Added meta tags to blog posts.</li>
