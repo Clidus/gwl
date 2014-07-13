1d62ee53da38094942cbe7708c6ff2800d0f6e64
Joshua Marketis <josh@clidus.com>
2014-07-13 16:08:24 +0100

    Move page logic into Ignition class

---
+++ ignition_application/models/ignition/page.php
@@ -0,0 +1,52 @@
+<?php 
+
+/*
+|--------------------------------------------------------------------------
+| Ignition v0.1 ignitionpowered.co.uk
+|--------------------------------------------------------------------------
+|
+| This class is a core part of Ignition. It is advised that you extend
+| this class rather than modifying it, unless you wish to contribute
+| to the project.
+|
+*/
+
+class IG_Page extends CI_Model {
+
+    function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+    }
+
+    // create data for page view
+    function create($pageTitle, $pageTemplate)
+    {
+        $data['pagetitle'] = $pageTitle;
+        $data['pagetemplate'] = $pageTemplate;
+        $data['sessionUserID'] = $this->session->userdata('UserID');
+        $data['sessionUsername'] = $this->session->userdata('Username');
+        $data['sessionAdmin'] = $this->session->userdata('Admin');
+        $data['sessionProfileImage'] = $this->session->userdata('ProfileImage');
+        $data['metaTags'] = null;
+
+        return $data; 
+    }
+
+    // create meta tags for blog
+    function getBlogMetaTags($post)
+    {
+        return '<meta name="description" content="' . $post->Deck . '" />
+        <link rel="author" href="https://plus.google.com/+' . $post->GooglePlus . '"/>
+        <link rel="publisher" href="https://plus.google.com/b/116486371787813419091/+Gamingwithlemons"/>
+        <meta itemprop="name" content="' . $post->Title . '"> 
+        <meta itemprop="description" content="' . $post->Deck . '"> 
+        <meta itemprop="image" content="' . $post->Image . '">
+        <meta property="og:title" content="' . $post->Title . '" /> 
+        <meta property="og:type" content="article" /> 
+        <meta property="og:url" content="' . base_url() . 'blog/' . $post->URL . '" />
+        <meta property="og:image" content="' . base_url() . $post->Image . '" />
+        <meta property="og:description" content="' . $post->Deck . '" /> 
+        <meta property="og:site_name" content="Gaming with Lemons" /> ';
+    }
+}
--- ignition_application/models/page.php
+++ ignition_application/models/page.php
@@ -1,41 +1,22 @@
 <?php 
 
-class Page extends CI_Model {
+/*
+|--------------------------------------------------------------------------
+| Ignition v0.1 ignitionpowered.co.uk
+|--------------------------------------------------------------------------
+|
+| This class extends the functionality of Ignition. You can add your
+| own custom logic here.
+|
+*/
+
+require_once APPPATH.'/models/ignition/page.php';
+
+class Page extends IG_Page {
 
     function __construct()
     {
         // Call the Model constructor
         parent::__construct();
-    }
-
-    // create data for page view
-    function create($pageTitle, $pageTemplate)
-    {
-        $data['pagetitle'] = $pageTitle;
-        $data['pagetemplate'] = $pageTemplate;
-        $data['sessionUserID'] = $this->session->userdata('UserID');
-        $data['sessionUsername'] = $this->session->userdata('Username');
-        $data['sessionAdmin'] = $this->session->userdata('Admin');
-        $data['sessionProfileImage'] = $this->session->userdata('ProfileImage');
-        $data['metaTags'] = null;
-
-        return $data; 
-    }
-
-    // create meta tags for blog
-    function getBlogMetaTags($post)
-    {
-        return '<meta name="description" content="' . $post->Deck . '" />
-        <link rel="author" href="https://plus.google.com/+' . $post->GooglePlus . '"/>
-        <link rel="publisher" href="https://plus.google.com/b/116486371787813419091/+Gamingwithlemons"/>
-        <meta itemprop="name" content="' . $post->Title . '"> 
-        <meta itemprop="description" content="' . $post->Deck . '"> 
-        <meta itemprop="image" content="' . $post->Image . '">
-        <meta property="og:title" content="' . $post->Title . '" /> 
-        <meta property="og:type" content="article" /> 
-        <meta property="og:url" content="' . base_url() . 'blog/' . $post->URL . '" />
-        <meta property="og:image" content="' . base_url() . $post->Image . '" />
-        <meta property="og:description" content="' . $post->Deck . '" /> 
-        <meta property="og:site_name" content="Gaming with Lemons" /> ';
     }
 }
