67f328744097f4e24a960369117555cdcc447053
Joshua Marketis <josh@clidus.com>
2015-04-04 22:49:22 +0100

    Add error when no platforms #36

--- ignition_application/views/user/platforms.php
+++ ignition_application/views/user/platforms.php
@@ -1,39 +1,44 @@
 		<?php
-			foreach($platforms as $platform)
+			if(count($platforms) == 0)
 			{
-				echo '<div class="row">
-					<div class="col-xs-12">
-						<p><a href="">' . $platform->Name . '</a></p>
+				echo "<div class='alert alert-warning'>No games found handsome.</div>";
+			} else {
+				foreach($platforms as $platform)
+				{
+					echo '<div class="row">
+						<div class="col-xs-12">
+							<p><a href="">' . $platform->Name . '</a></p>
+						</div>
 					</div>
-				</div>
 
-				<div class="row collectionStats">
-					<div class="col-xs-4">
-						<a href=""><img src="/images/platforms/' . $platform->Image . '" class="imageShadow platformLogo" /></a>
+					<div class="row collectionStats">
+						<div class="col-xs-4">
+							<a href=""><img src="/images/platforms/' . $platform->Image . '" class="imageShadow platformLogo" /></a>
+						</div>
+						<div class="col-xs-2">
+							<span>' . $platform->Collection . '</span>
+							<p>Collection</p>
+						</div>
+						<div class="col-xs-2">
+							<span>' . $platform->Completed . '</span>
+							<p>Completed</p>
+						</div>
+						<div class="col-xs-2">
+							<span>' . $platform->Backlog . '</span>
+							<p>Backlog</p>
+						</div>
+						<div class="col-xs-2">
+							<span>' . $platform->Want . '</span>
+							<p>Want</p>
+						</div>
 					</div>
-					<div class="col-xs-2">
-						<span>' . $platform->Collection . '</span>
-						<p>Collection</p>
-					</div>
-					<div class="col-xs-2">
-						<span>' . $platform->Completed . '</span>
-						<p>Completed</p>
-					</div>
-					<div class="col-xs-2">
-						<span>' . $platform->Backlog . '</span>
-						<p>Backlog</p>
-					</div>
-					<div class="col-xs-2">
-						<span>' . $platform->Want . '</span>
-						<p>Want</p>
-					</div>
-				</div>
 
-				<div class="progress">
-					<div style="width: 0%" class="progress-bar progress-bar-success" role="progressbar" aria-valuemin="0" aria-valuemax="100" data-percentage="' . $platform->Percentage . '">
-						' . $platform->Percentage . '% Complete
-					</div>
-				</div>';
+					<div class="progress">
+						<div style="width: 0%" class="progress-bar progress-bar-success" role="progressbar" aria-valuemin="0" aria-valuemax="100" data-percentage="' . $platform->Percentage . '">
+							' . $platform->Percentage . '% Complete
+						</div>
+					</div>';
+				}
 			}
 		?>
 	</div>
