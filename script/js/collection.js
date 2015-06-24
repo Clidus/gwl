6baf1e7fd950987ee00effdce35e20d2e4af47c3
Joshua Marketis <josh@clidus.com>
2015-06-24 23:18:04 +0100

    Render basic collection list #55

--- script/js/collection.js
+++ script/js/collection.js
@@ -1,17 +1,60 @@
-var GameCollection = React.createClass({displayName: "GameCollection",
+var filters = { lists : [], statuses : [], platforms : [], includeNoPlatforms : true, orderBy: "releaseDateDesc" };
+var currentPage = 1;
+
+var GameCollectionApp = React.createClass({displayName: "GameCollectionApp",
+    getInitialState: function(){
+        return { collection: [] };
+    },
+    componentDidMount: function() {
+        this.getCollection();
+    },
+    getCollection: function() {
+        $.ajax({
+            type : 'POST',
+            url : '/user/getCollection',
+            dataType : 'json',
+            data: {
+                userID: UserID,
+                page: currentPage,
+                filters: JSON.stringify(filters)
+            },
+            success: function(data) {
+                this.setProps({ collection: data.collection });
+                console.log(data.collection);
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
     render: function() {
-        console.log("React!");
         return (
-            React.createElement("div", null, 
-                React.createElement("h1", null, "Video Games!")
-            )
+            React.createElement(GameList, {games: this.props.collection})
         );
+    }
+});
+
+var GameList = React.createClass({displayName: "GameList",
+    render: function() {
+        if (this.props.games) {
+            return (
+                React.createElement("ul", null, 
+                    this.props.games.map(function(game, i) {
+                      return (
+                        React.createElement("li", {key: game.GBID}, game.Name)
+                      );
+                    }, this)
+                )
+            );
+        } else {
+            return null;
+        }
     }
 });
 
 function loadCollection(){
     React.render(
-        React.createElement(GameCollection, null),
+        React.createElement(GameCollectionApp, null),
         document.getElementById('gameCollection')
     );
 }
--- script/jsx/collection.js
+++ script/jsx/collection.js
@@ -1,17 +1,60 @@
-var GameCollection = React.createClass({
+var filters = { lists : [], statuses : [], platforms : [], includeNoPlatforms : true, orderBy: "releaseDateDesc" };
+var currentPage = 1;
+
+var GameCollectionApp = React.createClass({
+    getInitialState: function(){
+        return { collection: [] };
+    },
+    componentDidMount: function() {
+        this.getCollection();
+    },
+    getCollection: function() {
+        $.ajax({
+            type : 'POST',
+            url : '/user/getCollection',
+            dataType : 'json',
+            data: {
+                userID: UserID,
+                page: currentPage,
+                filters: JSON.stringify(filters)
+            },
+            success: function(data) {
+                this.setProps({ collection: data.collection });
+                console.log(data.collection);
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
     render: function() {
-        console.log("React!");
         return (
-            <div>
-                <h1>Video Games!</h1>
-            </div>
+            <GameList games={this.props.collection} />
         );
+    }
+});
+
+var GameList = React.createClass({
+    render: function() {
+        if (this.props.games) {
+            return (
+                <ul>
+                    {this.props.games.map(function(game, i) {
+                      return (
+                        <li key={game.GBID}>{game.Name}</li>
+                      );
+                    }, this)}
+                </ul>
+            );
+        } else {
+            return null;
+        }
     }
 });
 
 function loadCollection(){
     React.render(
-        <GameCollection />,
+        <GameCollectionApp />,
         document.getElementById('gameCollection')
     );
 }
